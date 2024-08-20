160b2da5fd315562ee99dc24c11fee1721003340
Chris Breunig <chrisbreunig2311@gmail.com>
2024-08-20 21:32:38 +0200

    ghfh

--- health-desk/src/database/MySQL/db.js
+++ health-desk/src/database/MySQL/db.js
@@ -7,12 +7,7 @@ async function getMySQLConnection() {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         multipleStatements: process.env.DB_MULTIPLE_STATEMENTS === 'true',
-<<<<<<< HEAD
-        database: process.env.DB_NAME,
-
-=======
         database: process.env.DB_NAME
->>>>>>> cd85a4846ea21352672165bc646a0911501bb8ab
     });
 
     return connection;
